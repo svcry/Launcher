899360e8894254d95146f9b85711ecae7c27c601
Akimov Aleksey <svcrifice@yandex.ru>
2023-03-31 15:50:57 +0700

    Добавьте файлы проекта.

---
+++ Launcher/ViewModel/MainViewModel.cs
@@ -0,0 +1,83 @@
+﻿using GalaSoft.MvvmLight.Command;
+using Launcher.View;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Launcher.ViewModel
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        private Page _currPage = new WowPage();
+        private Page Wow = new WowPage();
+        private Page W3 = new W3Page();
+        private Page Sc = new SCPage();
+        private Page Sc2 = new SC2Page();
+        private Page Ow = new OverwatchPage();
+        private Page Hs = new HSPage();
+        private Page Hots = new HOTSPage();
+        private Page Diablo = new DiabloPage();
+
+        public Page CurrPage
+        {
+            get => _currPage;
+            set => Set(ref _currPage, value);
+        }
+
+        public ICommand OpenHSPage
+        {
+            get 
+            {
+                return new RelayCommand(() => CurrPage = Hs);
+            }
+        }
+        public ICommand OpenWowPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurrPage = Wow);
+            }
+        }
+        public ICommand OpenSCPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurrPage = Sc);
+            }
+        }
+        public ICommand OpenSC2Page
+        {
+            get
+            {
+                return new RelayCommand(() => CurrPage = Sc2);
+            }
+        }
+        public ICommand OpenHOTSPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurrPage = Hots);
+            }
+        }
+        public ICommand OpenOWPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurrPage = Ow);
+            }
+        }
+        public ICommand OpenDiabloPage
+        {
+            get
+            {
+                return new RelayCommand(() => CurrPage = Diablo);
+            }
+        }
+        public ICommand OpenW3Page
+        {
+            get
+            {
+                return new RelayCommand(() => CurrPage = W3);
+            }
+        }
+    }
+}
---
+++ Launcher/ViewModel/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher.ViewModel
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void onPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            onPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
